90ecd859c13ed649a7b02801938e1668b32c39e7
Janith Madarasinghe <73998693+Janithpm@users.noreply.github.com>
2024-06-25 12:16:42 +0530

    ec2

--- get_timetable.py
+++
@@ -1,5 +0,0 @@
-from timetable import getTimetableData
-
-timetable = getTimetableData(6847)
-with open('data/timetable.json', 'w') as tf:
-    tf.write(timetable)
--- timetable.py
+++
@@ -1,6 +0,0 @@
-from bus_API import fetchData
-
-def getTimetableData(datasetID):
-    url = f"https://data.bus-data.dft.gov.uk/api/v1/dataset/{datasetID}"
-    return fetchData(url)
-    
